63deebe4b3ebd5c55faedd0f3ad1c55487b2220c
kartikay-agarwal <73934694+kartikay-agarwal@users.noreply.github.com>
2022-03-03 02:44:06 +0530

    Create gui.sh

---
+++ gui.sh
@@ -0,0 +1,25 @@
+sudo apt update
+
+sudo apt-get install -y wget
+
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+
+sudo apt install -y --fix-broken
+
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+
+sudo apt install -y --fix-broken
+
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+
+sudo apt install -y xscreensaver
+
+sudo apt install -y task-xfce-desktop
+
+sudo usermod -a -G chrome-remote-desktop $USER
+
+CRP=""
+
+{CRP} --pin=123456
