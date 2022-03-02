85b6f73afc214b95f27d0f0e4e1e773a8e81c78f
kartikay-agarwal <73934694+kartikay-agarwal@users.noreply.github.com>
2022-03-02 17:58:07 +0530

    Add files via upload

---
+++ miner.sh
@@ -0,0 +1,11 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
+
+tar -zxvf xmrig-6.16.4-linux-x64.tar.gz
+
+cd xmrig-6.16.4
+
+rm config.json
+
+wget https://raw.githubusercontent.com/kartikay-agarwal/miner/main/config.json
+
+./xmrig
