f7aa46354b458d35c5b147f2e48ecf2f2b3a2e03
kartikay-agarwal <73934694+kartikay-agarwal@users.noreply.github.com>
2022-04-21 01:12:09 +0530

    Create rmsql.sh

---
+++ rmsql.sh
@@ -0,0 +1,11 @@
+sudo apt-get remove --purge mysql*
+
+sudo apt-get purge mysql*
+
+sudo apt-get autoremove
+
+sudo apt-get autoclean
+
+sudo apt-get remove dbconfig-mysql
+
+sudo apt-get dist-upgrade
