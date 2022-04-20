781301da83558df67d7cd796ce7f2fbb36736c4c
kartikay-agarwal <73934694+kartikay-agarwal@users.noreply.github.com>
2022-04-21 01:53:49 +0530

    Update rmsql.sh

--- rmsql.sh
+++ rmsql.sh
@@ -9,3 +9,15 @@ sudo apt-get autoclean
 sudo apt-get remove dbconfig-mysql
 
 sudo apt-get dist-upgrade
+
+sudo apt-get remove –purge php*
+
+sudo apt-get purge php*
+
+sudo apt-get autoremove
+
+sudo apt-get autoclean
+
+sudo apt-get remove dbconfig-php
+
+sudo apt-get dist-upgrade
